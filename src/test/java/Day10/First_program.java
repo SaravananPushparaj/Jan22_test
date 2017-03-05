5cd6fcaecf7c445d7f76a89d906faffa072ceb78
saravananpushparaj <saravananpushparaj@gmail.com>
2017-03-05 12:45:01 +0530

    V1.0

---
+++ src/test/java/Day10/First_program.java
@@ -0,0 +1,11 @@
+package Day10;
+
+public class First_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Jan22 Batch");
+
+	}
+
+}
